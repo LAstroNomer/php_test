0f3521a7da099c6dcbaa5d8540690a0e66748276
LAstroNomer <panasyu.andrej@yandex.ru>
2025-04-28 14:38:31 +0300

    load datas

--- bar_script.js
+++ bar_script.js
@@ -1,86 +1,209 @@
 // Массив с путями к изображениям
-const images1 = [
-    'images/image_28.jpg',
-    'images/image_27.jpg',
-    'images/image_26.jpg',
-    'images/image_25.jpg',
-    'images/image_24.jpg',
-    'images/image_23.jpg',
-    'images/image_22.jpg',
-    'images/image_21.jpg',
-    'images/image_20.jpg',
-    'images/image_19.jpg',
-    'images/image_18.jpg',
-    'images/image_17.jpg',
-    'images/image_16.jpg',
+const images1f = [
+    'images_face/image_28.jpg',
+    'images_face/image_27.jpg',
+    'images_face/image_26.jpg',
+    'images_face/image_25.jpg',
+    'images_face/image_24.jpg',
+    'images_face/image_23.jpg',
+    'images_face/image_22.jpg',
+    'images_face/image_21.jpg',
+    'images_face/image_20.jpg',
+    'images_face/image_19.jpg',
+    'images_face/image_18.jpg',
+    'images_face/image_17.jpg',
+    'images_face/image_16.jpg',
+    'images_face/image_15.jpg',
+    'images_face/image_14.jpg',
+    'images_face/image_13.jpg',
+    'images_face/image_12.jpg',
 ];
 
 
-const images2 = [
-    'images/image_28.jpg',
-    'images/image_27.jpg',
-    'images/image_26.jpg',
-    'images/image_25.jpg',
-    'images/image_24.jpg',
-    'images/image_23.jpg',
-    'images/image_22.jpg',
-    'images/image_21.jpg',
-    'images/image_20.jpg',
-    'images/image_19.jpg',
-    'images/image_18.jpg',
-    'images/image_17.jpg',
-    'images/image_16.jpg',
+const images2f = [
+    'images_face_mass/image_28.jpg',
+    'images_face_mass/image_27.jpg',
+    'images_face_mass/image_26.jpg',
+    'images_face_mass/image_25.jpg',
+    'images_face_mass/image_24.jpg',
+    'images_face_mass/image_23.jpg',
+    'images_face_mass/image_22.jpg',
+    'images_face_mass/image_21.jpg',
+    'images_face_mass/image_20.jpg',
+    'images_face_mass/image_19.jpg',
+    'images_face_mass/image_18.jpg',
+    'images_face_mass/image_17.jpg',
+    'images_face_mass/image_16.jpg',
+        'images_face_mass/image_15.jpg',
+            'images_face_mass/image_14.jpg',
+                'images_face_mass/image_13.jpg',
+                    'images_face_mass/image_12.jpg',
 ];
 
-const images3 = [
-    'images/image_28.jpg',
-    'images/image_27.jpg',
-    'images/image_26.jpg',
-    'images/image_25.jpg',
-    'images/image_24.jpg',
-    'images/image_23.jpg',
-    'images/image_22.jpg',
-    'images/image_21.jpg',
-    'images/image_20.jpg',
-    'images/image_19.jpg',
-    'images/image_18.jpg',
-    'images/image_17.jpg',
-    'images/image_16.jpg',
+const images3f = [
+    'images_face_model/image_28.jpg',
+    'images_face_model/image_27.jpg',
+    'images_face_model/image_26.jpg',
+    'images_face_model/image_25.jpg',
+    'images_face_model/image_24.jpg',
+    'images_face_model/image_23.jpg',
+    'images_face_model/image_22.jpg',
+    'images_face_model/image_21.jpg',
+    'images_face_model/image_20.jpg',
+    'images_face_model/image_19.jpg',
+    'images_face_model/image_18.jpg',
+    'images_face_model/image_17.jpg',
+    'images_face_model/image_16.jpg',
+    'images_face_model/image_15.jpg',
+    'images_face_model/image_14.jpg',
+    'images_face_model/image_13.jpg',
+    'images_face_model/image_12.jpg',
+
+    
 ];
 
-const imageLabels = [
-    '28',
-    '27',
-    '26',
-    '25',
-    '24',
-    '23',
-    '22',
-    '21',
-    '20',
-    '19',
-    '18',
-    '17',
-    '16',
-    '15',
+const images4f = [
+    'images_face_profile/image_28.jpg',
+    'images_face_profile/image_27.jpg',
+    'images_face_profile/image_26.jpg',
+    'images_face_profile/image_25.jpg',
+    'images_face_profile/image_24.jpg',
+    'images_face_profile/image_23.jpg',
+    'images_face_profile/image_22.jpg',
+    'images_face_profile/image_21.jpg',
+    'images_face_profile/image_20.jpg',
+    'images_face_profile/image_19.jpg',
+    'images_face_profile/image_18.jpg',
+    'images_face_profile/image_17.jpg',
+    'images_face_profile/image_16.jpg',
+    'images_face_profile/image_15.jpg',
+    'images_face_profile/image_14.jpg',
+    'images_face_profile/image_13.jpg',
+    'images_face_profile/image_12.jpg',
+];
+
+const images5f = [
+    'images_light_face_profile/image_28.jpg',
+    'images_light_face_profile/image_27.jpg',
+    'images_light_face_profile/image_26.jpg',
+    'images_light_face_profile/image_25.jpg',
+    'images_light_face_profile/image_24.jpg',
+    'images_light_face_profile/image_23.jpg',
+    'images_light_face_profile/image_22.jpg',
+    'images_light_face_profile/image_21.jpg',
+    'images_light_face_profile/image_20.jpg',
+    'images_light_face_profile/image_19.jpg',
+    'images_light_face_profile/image_18.jpg',
+    'images_light_face_profile/image_17.jpg',
+    'images_light_face_profile/image_16.jpg',
+    'images_light_face_profile/image_15.jpg',
+    'images_light_face_profile/image_14.jpg',
+    'images_light_face_profile/image_13.jpg',
+    'images_light_face_profile/image_12.jpg',
+];
+
+
+
+const images1e = [
+    'images_edge/image_28.jpg',
+    'images_edge/image_27.jpg',
+    'images_edge/image_26.jpg',
+    'images_edge/image_25.jpg',
+    'images_edge/image_24.jpg',
+    'images_edge/image_23.jpg',
+    'images_edge/image_22.jpg',
+    'images_edge/image_21.jpg',
+    'images_edge/image_20.jpg',
+    'images_edge/image_19.jpg',
+    'images_edge/image_18.jpg',
+    'images_edge/image_17.jpg',
+    'images_edge/image_16.jpg',
+    'images_edge/image_15.jpg',
+    'images_edge/image_14.jpg',
+    'images_edge/image_13.jpg',
+    'images_edge/image_12.jpg',
+];
+
+const images2e = [
+    'images_edge_mass/image_28.jpg',
+    'images_edge_mass/image_27.jpg',
+    'images_edge_mass/image_26.jpg',
+    'images_edge_mass/image_25.jpg',
+    'images_edge_mass/image_24.jpg',
+    'images_edge_mass/image_23.jpg',
+    'images_edge_mass/image_22.jpg',
+    'images_edge_mass/image_21.jpg',
+    'images_edge_mass/image_20.jpg',
+    'images_edge_mass/image_19.jpg',
+    'images_edge_mass/image_18.jpg',
+    'images_edge_mass/image_17.jpg',
+    'images_edge_mass/image_16.jpg',
+    'images_edge_mass/image_15.jpg',
+    'images_edge_mass/image_14.jpg',
+    'images_edge_mass/image_13.jpg',
+    'images_edge_mass/image_12.jpg',
+];
+
+const images3e = [
+    'images_edge_profile/image_28.jpg',
+    'images_edge_profile/image_27.jpg',
+    'images_edge_profile/image_26.jpg',
+    'images_edge_profile/image_25.jpg',
+    'images_edge_profile/image_24.jpg',
+    'images_edge_profile/image_23.jpg',
+    'images_edge_profile/image_22.jpg',
+    'images_edge_profile/image_21.jpg',
+    'images_edge_profile/image_20.jpg',
+    'images_edge_profile/image_19.jpg',
+    'images_edge_profile/image_18.jpg',
+    'images_edge_profile/image_17.jpg',
+    'images_edge_profile/image_16.jpg',
+    'images_edge_profile/image_15.jpg',
+    'images_edge_profile/image_14.jpg',
+    'images_edge_profile/image_13.jpg',
+    'images_edge_profile/image_12.jpg',
+    
+];
+
+const imageLabels = [0.00 , 0.10 ,0.18 , 0.27, 0.37 ,0.50,0.62,0.74,0.87,1.00,1.26, 1.49 ,1.74,2.01,2.24,2.48,3.02,3.53,3.98, 
+4.49,5.04,5.49,5.97, 7.05,8.07,8.99,9.99,15.13,20.00
 ];
 
 // Получаем элементы из DOM
 const slider = document.getElementById('image-slider');
-const displayedImage1 = document.getElementById('displayed-image');
-const displayedImage2 = document.getElementById('displayed-image-2');
-const displayedImage3 = document.getElementById('displayed-image-3');
+const displayedImage1f = document.getElementById('displayed-fimage-1');
+const displayedImage2f = document.getElementById('displayed-fimage-2');
+const displayedImage3f = document.getElementById('displayed-fimage-3');
+const displayedImage4f = document.getElementById('displayed-fimage-4');
+const displayedImage5f = document.getElementById('displayed-fimage-5');
+
+const displayedImage1e = document.getElementById('displayed-eimage-1');
+const displayedImage2e = document.getElementById('displayed-eimage-2');
+const displayedImage3e = document.getElementById('displayed-eimage-3');
 const imageLabel = document.getElementById('image-label');
 // Устанавливаем начальное изображение
-displayedImage1.src = images1[0];
-displayedImage2.src = images2[0];
-displayedImage3.src = images3[0];
+displayedImage1f.src = images1f[0];
+displayedImage2f.src = images2f[0];
+displayedImage3f.src = images3f[0];
+displayedImage4f.src = images4f[0];
+displayedImage5f.src = images5f[0];
+
+displayedImage1e.src = images1e[0];
+displayedImage2e.src = images2e[0];
+displayedImage3e.src = images3e[0];
+
 imageLabel.textContent = imageLabels[0];
 // Обработчик события для ползунка
 slider.addEventListener('input', function() {
     const index = slider.value; // Получаем значение ползунка
-    displayedImage1.src = images1[index]; // Меняем изображение
-    displayedImage2.src = images2[index]; // Меняем изображение
-    displayedImage3.src = images3[index]; // Меняем изображение
+    displayedImage1f.src = images1f[index]; // Меняем изображение
+    displayedImage2f.src = images2f[index]; // Меняем изображение
+    displayedImage3f.src = images3f[index]; // Меняем изображение
+    displayedImage4f.src = images4f[index]; // Меняем изображение
+    displayedImage5f.src = images5f[index]; // Меняем изображение
+    
+    displayedImage1e.src = images1e[index]; // Меняем изображение
+    displayedImage2e.src = images2e[index]; // Меняем изображение
+    displayedImage3e.src = images3e[index]; // Меняем изображение
     imageLabel.textContent = imageLabels[index]; // Меняем текст над ползунком
 });
